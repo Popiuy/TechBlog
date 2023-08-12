2ab6d9ca9d3e4dbbf0ca0c8dcb1b3f9f75320830
Popiuy <rymaxpayne@gmail.com>
2023-08-11 21:47:31 -0700

    testing heroku

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,25 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
-router.post('/', async (req, res) => {
-  try {
-    const userData = await User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
-
-    req.session.name = userData.name;
-    req.session.user_id = userData.id;
-    req.session.logged_in = true;
-    req.session.save(() => {
-      res.status(200).json(userData);
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
-
 router.post('/login', async (req, res) => {
   try {
     const userData = await User.findOne({ where: { email: req.body.email } });
